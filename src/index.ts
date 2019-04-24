5d5308ce827eefea01e01693f8ea019c5515a99e
Vincent Desdoigts <v.desdoigts@gmail.com>
2019-04-24 12:39:59 +0200

    Fix data-toggle button on mobile

--- src/index.ts
+++ src/index.ts
@@ -1686,9 +1686,10 @@ function FlatpickrInstance(
         e.target && (e.target as HTMLInputElement).blur();
       }
 
-      setTimeout(() => {
-        self.mobileInput !== undefined && self.mobileInput.focus();
-      }, 0);
+      if (self.mobileInput !== undefined) {
+        self.mobileInput.focus();
+        self.mobileInput.click();
+      }
 
       triggerEvent("onOpen");
       return;
